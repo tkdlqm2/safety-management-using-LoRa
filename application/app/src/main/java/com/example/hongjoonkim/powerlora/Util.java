b6c4c507f37a3517655b61f78983b0fdcc8d5db5
tkdlqm2 <tkdlqm2@naver.com>
2020-01-01 16:40:09 +0900

    [2020 01 01] 안드로이드 김홍준

--- application/app/src/main/java/com/example/hongjoonkim/powerlora/Util.java
+++ application/app/src/main/java/com/example/hongjoonkim/powerlora/Util.java
@@ -5,9 +5,9 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
 public class Util {
-    public static final String REGISTER_URL = "http://15.164.218.15:8080/mobile2/list.jsp";
-    public static final String REGISTER_URL2 = "http://15.164.218.15:8080/mobile2/";
-    public static final String REGISTER_URL3 = "15.164.218.15:8080";
+    public static final String REGISTER_URL = "http://52.79.109.78:8080/mobile2/list.jsp";
+    public static final String REGISTER_URL2 = "http://52.79.109.78:8080/mobile2/";
+    public static final String REGISTER_URL3 = "52.79.109.78:8080";
 
     public static void showWarning(AppCompatActivity cmp, String msg)
     {
